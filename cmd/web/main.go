672d90dc1e3e554851d3df5ceee8a54195dd894c
Артур Низамитдинов <anezamus-10@mail.ru>
2025-02-26 19:59:35 +0300

    Add middleware logger to all mux

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -21,7 +21,6 @@ func main() {
 
 
 	log.Printf("Server is listening on %s ...", srv.Addr)
-
 	if err := srv.ListenAndServe(); err != nil {
 		log.Fatalf("Ошибка при запуске сервера %s", err.Error())
 	}
