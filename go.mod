90265a548c2e386ef566cdb87a049c9a72d9459f
Артур Низамитдинов <anezamus-10@mail.ru>
2025-03-04 18:19:13 +0300

    Add tests to Home and GetSmokers

--- go.mod
+++ go.mod
@@ -3,3 +3,10 @@ module github.com/NarthurN/QuitSmoking
 go 1.23.4
 
 require github.com/go-chi/chi/v5 v5.2.1
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.10.0
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ internal/handlers/handlers_test.go
@@ -0,0 +1,37 @@
+package handlers
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+
+func TestHomeWhenOk(t *testing.T) {
+	r := httptest.NewRequest("GET", "/", nil)
+	h := New(nil, nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(h.Home())
+	handler.ServeHTTP(responseRecorder, r)
+	
+	assert.Equal(t, responseRecorder.Code, http.StatusOK)
+
+	expectedText := "Это приложение для тех, кто бросает курить!"
+	body := responseRecorder.Body.String()
+	assert.Equal(t, expectedText, body)
+}
+
+func TestGetSmokersWhenOk(t *testing.T)	{
+	r := httptest.NewRequest("GET", "/smokers", nil)
+	h := New(nil, nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(h.GetSmokers())
+	handler.ServeHTTP(responseRecorder, r)
+	
+	assert.Equal(t, responseRecorder.Code, http.StatusOK)
+
+}
